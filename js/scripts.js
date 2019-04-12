31142b2394c6f26ebe72db40a8b1457bd84727d3
Anna Gibbons <anna@gibbons-clan.com>
2019-04-12 12:32:14 -0700

    add more js functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
-function Orders {
-  this.pizza = []
+function Orders() {
+  this.pizzaOrders = []
+  this.currentId = 0
 }
 
-Orders.prototype.addPizzaOrder =
-function(pizzaOrder) {
+Orders.prototype.addPizzaOrder = function(pizzaOrder) {
+  pizzaOrder.id = thisassignId();
   this.pizzaOrders.push(pizzaOrder);
 }
 
-function pizzaOrder (firstName, phoneNumber,size,toppings,crust) {
+Orders.prototype.assignId = function (pizzaOrder) {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+function pizzaOrder (firstName, phoneNumber,size,toppings,crust,cost) {
   this.firstName: firstName,
   this.phoneNumber,
   this.size: size,
   this.toppings: toppings,
   this.crust: crust,
+  this.cost: cost,
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   $("form#pizza-creator").submit(funtion(event) {
     event.preventDefault();
 
