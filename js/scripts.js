f95a8cea04ccbadba2143466e9d03d61a2e4d697
Anna Gibbons <anna@gibbons-clan.com>
2019-04-12 11:31:09 -0700

    add js objects pizzaOrder and Orders, along with function addPizzaOrder.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
-pizza {
-  size: size,
-  toppings: toppings,
-  cheese: cheese,
-  crust: crust,
+function Orders {
+  this.pizza = []
+}
+
+Orders.prototype.addPizzaOrder =
+function(pizzaOrder) {
+  this.pizzaOrders.push(pizzaOrder);
+}
+
+function pizzaOrder (firstName, phoneNumber,size,toppings,crust) {
+  this.firstName: firstName,
+  this.phoneNumber,
+  this.size: size,
+  this.toppings: toppings,
+  this.crust: crust,
 }
 
 $(document).ready(function(){
